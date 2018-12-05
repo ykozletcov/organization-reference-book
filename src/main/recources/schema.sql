dfaf0baa552db286b52576de5f62bea087c1bac8
ykozletcov <39530375+ykozletcov@users.noreply.github.com>
2018-12-05 19:57:19 +0400

    Update schema.sql

--- src/main/recources/schema.sql
+++ src/main/recources/schema.sql
@@ -62,15 +62,11 @@ CREATE TABLE IF NOT EXISTS employee (
     last_name       VARCHAR(50)     NOT NULL,
     position_id     INTEGER         NOT NULL,
     phone           VARCHAR(18),
-    doc_id          INTEGER         NOT NULL,
     citizenship_id  INTEGER         NOT NULL,
-    office_id       INTEGER         NOT NULL,
     is_identified   SMALLINT
 );
 ALTER TABLE employee    ADD FOREIGN KEY (position_id)         REFERENCES position_list(id);
-ALTER TABLE employee    ADD FOREIGN KEY (doc_id)              REFERENCES doc(id);
 ALTER TABLE employee    ADD FOREIGN KEY (citizenship_id)      REFERENCES country_list(id);
-ALTER TABLE employee    ADD FOREIGN KEY (office_id)           REFERENCES office(id);
 
 CREATE INDEX IX_employee_first_name         ON employee(first_name);
 CREATE INDEX IX_employee_middle_name        ON employee(middle_name);
@@ -78,9 +74,7 @@ CREATE INDEX IX_employee_second_name        ON employee(second_name);
 CREATE INDEX IX_employee_last_name          ON employee(last_name);
 CREATE INDEX IX_employee_position_id        ON employee(position_id);
 CREATE INDEX IX_employee_phone              ON employee(phone);
-CREATE INDEX IX_employee_doc_id             ON employee(doc_id);
 CREATE INDEX IX_employee_citizenship_id     ON employee(citizenship_id);
-CREATE INDEX IX_employee_office_id          ON employee(office_id);
 CREATE INDEX IX_employee_is_identified      ON employee(is_identified);
 
 COMMENT ON TABLE employee                   IS 'информация о сотрудниках';
@@ -91,27 +85,45 @@ COMMENT ON COLUMN employee.second_name      IS 'второе имя сотруд
 COMMENT ON COLUMN employee.last_name        IS 'фамилия сотрудника';
 COMMENT ON COLUMN employee.position_id      IS 'внешний ключ - должность сотрудника';
 COMMENT ON COLUMN employee.phone            IS 'телефонный номер сотрудника';
-COMMENT ON COLUMN employee.doc_id           IS 'внешний ключ - документ сотрудника';
 COMMENT ON COLUMN employee.citizenship_id   IS 'внешний ключ - гражданство сотрудника';
-COMMENT ON COLUMN employee.office_id        IS 'внешний ключ - принадлежность офису';
 COMMENT ON COLUMN employee.is_identified    IS 'идентификация сотрудника';
+
+CREATE TABLE IF NOT EXISTS office_employee (
+    id              INTEGER PRIMARY KEY AUTO_INCREMENT,
+    employee_id     INTEGER         NOT NULL,
+    office_id       INTEGER         NOT NULL,
+);
+
+ALTER TABLE office_employee ADD FOREIGN KEY (employee_id)    REFERENCES employee(id);
+ALTER TABLE office_employee ADD FOREIGN KEY (office_id)      REFERENCES office(id);
+
+CREATE INDEX IX_office_employee_employee_id          ON office_employee(employee_id);
+CREATE INDEX IX_office_employee_office_id            ON office_employee(office_id);
+
+COMMENT ON TABLE office_employee                    IS 'отношение многие ко многим для офисов и сотрудников';
+COMMENT ON COLUMN office_employee.employee_id       IS 'внешний ключ - сотрудник';
+COMMENT ON COLUMN office_employee.office_id         IS 'внешний ключ - офис';
 
 
 
 CREATE TABLE IF NOT EXISTS doc (
     id              INTEGER PRIMARY KEY AUTO_INCREMENT,
+    employee_id     INTEGER         NOT NULL,
     doc_list_id     INTEGER         NOT NULL,
     number          VARCHAR(15)     NOT NULL,
     date            VARCHAR(10)     NOT NULL,
 );
+ALTER TABLE doc         ADD FOREIGN KEY (employee_id)         REFERENCES employee(id);
 ALTER TABLE doc         ADD FOREIGN KEY (doc_list_id)         REFERENCES doc_list(id);
 
+CREATE INDEX IX_employee_id                 ON doc(employee_id);
 CREATE INDEX IX_doc_doc_list_id             ON doc(doc_number);
 CREATE INDEX IX_doc_number                  ON doc(doc_number);
 CREATE INDEX IX_doc_date                    ON doc(doc_date);
 
 COMMENT ON TABLE doc                        IS 'информация о документах сотрудников';
 COMMENT ON COLUMN doc.id                    IS 'уникальный идентификатор документа';
+COMMENT ON COLUMN doc.employee_id           IS 'внешний ключ - принадлежность сотруднику';
 COMMENT ON COLUMN doc.doc_list_id           IS 'внешний ключ - вид документа';
 COMMENT ON COLUMN doc.number                IS 'номер документа';
 COMMENT ON COLUMN doc.date                  IS 'дата выдачи документа';
